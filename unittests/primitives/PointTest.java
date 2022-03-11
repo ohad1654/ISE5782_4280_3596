d2ecb1c44a121fa9fcf108cac044a79ba9aa64ac
Oz Diei <oz052777@gmail.com>
2022-03-11 13:34:50 +0200

    Base for TDD

---
+++ unittests/primitives/PointTest.java
@@ -0,0 +1,26 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PointTest {
+
+    @Test
+    public void testAdd() {
+    }
+
+    @Test
+    public void testSubtract() {
+    }
+
+    @Test
+    public void testDistanceSquared() {
+    }
+
+    @Test
+    public void testDistance() {
+    }
+
+
+}
---
+++ unittests/primitives/VectorTest.java
@@ -0,0 +1,46 @@
+package primitives;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VectorTest {
+
+    @Test
+    public void testAdd() {
+    }
+
+    @Test
+    public void testSubtract() {
+    }
+
+    @Test
+    public void testScaling() {
+    }
+
+    @Test
+    public void testDotProduct() {
+    }
+
+    @Test
+    public void testLength() {
+    }
+
+
+
+    @Test
+    public void testLengthSquared() {
+    }
+
+
+    @Test
+    public void testNormalize() {
+    }
+
+
+    @Test
+    public void testCrossProduct() {
+    }
+
+
+}
