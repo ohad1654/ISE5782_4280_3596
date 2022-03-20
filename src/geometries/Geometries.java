e9664c0458e4df06e42abd65960fb0afc2bec113
ohad1654 <ohadsc123@gmail.com>
2022-03-20 16:51:17 +0200

    Finish Geometries

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -3,17 +3,24 @@ package geometries;
 import primitives.Point;
 import primitives.Ray;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
 public class Geometries implements Intersectable{
-    LinkedList<Intersectable> shapes;
+    List<Intersectable> shapes;
     public Geometries(Intersectable... geometries)
     {
+        shapes= List.of(geometries);
     }
     @Override
     public List<Point> findIntersections(Ray ray) {
-        return null;
+        LinkedList<Point> intersectPoints = new LinkedList<>();
+        List<Point> shapePoint;
+        for (Intersectable shape:shapes) {
+            shapePoint=shape.findIntersections(ray);
+            if (shapePoint!=null)
+                intersectPoints.addAll(shapePoint);
+        }
+        return intersectPoints;
     }
 }
